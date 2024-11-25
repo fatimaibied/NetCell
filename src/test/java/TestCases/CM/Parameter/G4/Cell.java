16648919c239187e8613521e39ac4ca92cb1858d
LPAutomationQA <leadingpointjoqa@gmail.com>
2024-11-25 17:14:04 +0300

    first commit

--- src/test/java/TestCases/CM/Parameter/G4/Cell.java
+++ src/test/java/TestCases/CM/Parameter/G4/Cell.java
@@ -13,9 +13,4 @@ public class Cell extends MainClass {
 
         parameters.Parameter("4G", "Cell", "Last Week");
     }
-    @Test
-    public void CellParameter4G2() throws Exception {
-
-        parameters.Parameter("4G", "Cell", "Last Week");
-    }
 }
